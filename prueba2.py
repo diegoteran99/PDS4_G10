82502b348fcb99ceff207913ce2742b7ad58f9c0
diegoteran99 <diegoteranbaselli@gmail.com>
2022-11-06 23:33:57 -0300

    Game implemented

--- prueba2.py
+++ prueba2.py
@@ -2,26 +2,68 @@
 
 import random
 import telebot
-
+from telebot.types import Message
 bot = telebot.TeleBot("5662866930:AAEhXzJuXycvDthZMj4SyP43t__ch6EdqI4")
-
+global on_game
+on_game = False
 numbers = dict()
+users = dict()
+@bot.message_handler(commands=['start'])
+def greetings(message):
+    bot.send_message(message.chat.id, "/new_number <max_number> <tries> → Starting new number game\n (ex: /new_number 70 3)\n/stats → get all stats of the games")
 
-@bot.message_handler(commands=('start'))
+@bot.message_handler(commands=['new_number'])
 def guess_number(message):
-    numbers[message.chat.id] = random.randint(0, 100)
+    if on_game == False:
+        on_game = True
+        print("message: ", message.from_user.id)
+        print("message.entities: ", message.entities[0].type)
+        print("message: ", message.text.split(" ")[1])
+        max_number = int(message.text.split(" ")[1])
+        global tries
+        tries = int(message.text.split(" ")[2])
+        bot.send_message(message.chat.id, "Starting new game")
+        numbers[message.chat.id] = random.randint(0, max_number)
+    else:
+        bot.send_message(message.chat.id, "A game is alreday active")
+
+    
+       
+
 
 @bot.message_handler(func=lambda message: True)
 def checking(message):
-    try:
-        user_message = int(message.text)
-        if user_message > numbers[message.chat.id]:
-            bot.send_message(message.chat.id, "your number is greater than mine")
-        elif user_message < numbers[message.chat.id]:
-            bot.send_message(message.chat.id, "your number is smaller than mine")
+    if on_game == True:
+        if message.from_user.id not in users:
+            users[message.from_user.id] = tries - 1
         else:
-            bot.send_message(message.chat.id, "yep, that's the right number")
-    except ValueError:
-        bot.send_message(message.chat.id, "you must enter an integer")
-    except (TypeError, KeyError):
-        bot.send_message(message.chat.id, "there's no number to guess! say /start to start")
+            users[message.from_user.id] -= 1
+
+        try:
+            if users[message.from_user.id] > 0:
+                user_message = int(message.text)
+                print("numbers: ", numbers)
+                print("message.from_user: ", message.from_user)
+                print("user_message: ", user_message)
+                print("numbers[message.chat.id]: ", numbers[message.chat.id])
+                if user_message > numbers[message.chat.id]:
+                    bot.reply_to(message, "your number is greater than mine")
+                elif user_message < numbers[message.chat.id]:
+                    bot.reply_to(message, "your number is smaller than mine")
+                else:
+                    bot.reply_to(message, "yep, that's the right number\nThe winner is {}".format(message.from_user.first_name + " "+ message.from_user.last_name ))
+                    on_game = False
+                    
+            else:
+                bot.reply_to(message, "No more tries available")
+        except ValueError:
+            bot.send_message(message.chat.id, "you must enter an integer")
+        except (TypeError, KeyError):
+            bot.send_message(message.chat.id, "there's no number to guess! say /start to st")
+        
+        print("users: ", users) 
+        
+
+
+
+bot.infinity_polling()
